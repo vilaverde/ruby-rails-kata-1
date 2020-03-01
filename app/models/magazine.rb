9ee2749ef1ad27a093611f0e9f6b5f5c9727d058
musa <lucasvmachado@gmail.com>
2020-03-01 17:24:53 -0300

    Add magazine constraint - at least one author

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,4 +2,10 @@ class Magazine < ApplicationRecord
   validates :title, presence: true
   validates :published_at, presence: true
   validates :isbn, presence: true
+
+  validate :has_authors?
+
+  def has_authors?
+    errors.add(:authors, :required) if authors.blank?
+  end
 end
