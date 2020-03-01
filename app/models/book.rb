90db4c84aa79a791127871e4417b6a4603582f57
musa <lucasvmachado@gmail.com>
2020-03-01 17:19:21 -0300

    Add book constraint - at least one author

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,10 @@ class Book < ApplicationRecord
   validates :isbn, presence: true
 
   has_and_belongs_to_many :authors
+
+  validate :has_authors?
+
+  def has_authors?
+    errors.add(:authors, :required) if authors.blank?
+  end
 end
